2682a78fb71377342154ce2968eb55ff231cffdf
NIMMOOO <kumarvtiwari1973@gmail.com>
2024-06-27 15:01:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './components/sidebar/sidebar'
+import Main from './components/main/main'
+const App = () => {
+  return (
+  <>
+  <Sidebar/>
+  <Main/>
+
+    </>
+  )
+}
+export default App
